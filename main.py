1693e33689c64f5c65259e7a41c06934eb4d4b59
zdj1997 <zhangdejun1997@163.com>
2022-10-08 16:00:49 +0800

    d

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ import os
 import random
 from bs4 import BeautifulSoup
 
-
-today = datetime.now()
+today = datetime.now ()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
@@ -32,7 +31,23 @@ def get_iciba_everyday ():
     str = '【奇怪的知识】\n' + english + '\n' + zh_CN
     return str
 
-def get_weather_new():
+def getloverwords ():
+    texts = []
+    for i in range (1, int (random.randint (3, 83))):
+        url = 'https://www.duanwenxue.com/huayu/tianyanmiyu/list_{}.html'.format (i)
+        response = requests.get (url)
+        texts.append (response.text)
+    articles = []
+    for text in texts:
+        soup = BeautifulSoup (text, 'lxml')
+        arttis = soup.find ('div', class_ = 'list-short-article').find_all ('a', {'target': "_blank"})  # 寻找情话内容
+        #  通过列表推导式以及for循环获取到每个a标签里面的text内容并通过strip去除空格
+        articles.extend ([arttis[i].text.strip () for i in range (len (arttis))])
+    todaywords = articles[random.randint (0, len (articles) - 1)]  # 随机选取其中一条情话
+    return todaywords
+
+
+def get_weather_new ():
     tqurl = api + city_code
     response = requests.get (tqurl)
     d = response.json ()  # 将数据以json形式返回，这个d就是返回的json数据
@@ -65,58 +80,49 @@ def get_weather_new():
             'msg': tdwt.encode ('utf-8')
         }
         requests.post (cpurl, data = data)  # 把天气数据转换成UTF-8格式，不然要报错。
-    return parent,city,date,week,weather_type,weather_type,weather_type,shidupm25,pm10,quality,fx,fl,ganmao,tips,update_time
+    return parent, city, date, week, weather_type, weather_type, weather_type, shidu,pm25, pm10, quality, fx, fl, ganmao, tips, update_time
+
+
+def get_weather ():
+    url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+    res = requests.get (url).json ()
+    weather = res['data']['list'][0]
+    return weather['weather'], math.floor (weather['temp'])
+
+
+def get_count ():
+    delta = today - datetime.strptime (start_date, "%Y-%m-%d")
+    return delta.days
+
+
+def get_birthday ():
+    next = datetime.strptime (str (date.today ().year) + "-" + birthday, "%Y-%m-%d")
+    if next < datetime.now ():
+        next = next.replace (year = next.year + 1)
+    return (next - today).days
+
+
+def get_words ():
+    words = requests.get ("https://api.shadiao.pro/chp")
+    if words.status_code != 200:
+        return get_words ()
+    return words.json ()['data']['text']
 
 
 
+def get_random_color ():
+    return "#%06x" % random.randint (0, 0xFFFFFF)
 
 
+client = WeChatClient (app_id, app_secret)
 
-def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+wm = WeChatMessage (client)
+# wea, temperature = get_weather ()
+# data = {"weather": {"value": wea}, "temperature": {"value": temperature}, "love_days": {"value": get_count ()},
+#         "birthday_left": {"value": get_birthday ()}, "words": {"value": get_words (), "color": get_random_color ()}}
 
-def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
-
-def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
-
-def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
-
-def getloverwords ():
-    texts = []
-    for i in range (1, int (random.randint (3, 83))):
-        url = 'https://www.duanwenxue.com/huayu/tianyanmiyu/list_{}.html'.format (i)
-        response = requests.get (url)
-        texts.append (response.text)
-    articles = []
-    for text in texts:
-        soup = BeautifulSoup (text, 'lxml')
-        arttis = soup.find ('div', class_ = 'list-short-article').find_all ('a', {'target': "_blank"})  # 寻找情话内容
-        #  通过列表推导式以及for循环获取到每个a标签里面的text内容并通过strip去除空格
-        articles.extend ([arttis[i].text.strip () for i in range (len (arttis))])
-    todaywords = articles[random.randint (0, len (articles) - 1)]  # 随机选取其中一条情话
-    return todaywords
-
-def get_random_color():
-  return "#%06x" % random.randint(0, 0xFFFFFF)
-
-
-client = WeChatClient(app_id, app_secret)
-
-wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(user_id, template_id, data)
-print(res)
+parent, city, date, week, weather_type, shidu,pm25, pm10, quality, fx, fl, ganmao, tips, update_time=get_weather_new()
+lovewoed=getloverwords()
+data={'parent':parent,'city':city,'date':date,'week':week,'weather_type':weather_type,'shidu':shidu,'pm25':pm25,'pm10':pm10,'quality':quality,'fx':fx,'fl':fl,'ganmao':ganmao,'tips':tips,'update_time':update_time,'loveword':lovewoed}
+res = wm.send_template (user_id, template_id, data)
+print (res)
