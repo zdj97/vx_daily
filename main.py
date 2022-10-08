4572811ce771199a93fd0bb4b30e8210191a7bdb
zdj97 <113007896+zdj97@users.noreply.github.com>
2022-10-08 16:38:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -72,17 +72,17 @@ def get_weather_new ():
         fl = d["data"]["forecast"][0]["fl"]  # 风力
         ganmao = d["data"]["ganmao"]  # 感冒指数
         tips = d["data"]["forecast"][0]["notice"]  # 温馨提示
-        cpurl = 'https://qmsg.zendee.cn/send/' + spkey  # 自己改发送方式，我专门创建了个群来收消息，所以我用的group
+        # cpurl = 'https://qmsg.zendee.cn/send/' + spkey  # 自己改发送方式，我专门创建了个群来收消息，所以我用的group
         # 天气提示内容
         tdwt = get_iciba_everyday () + "\n-----------------------------------------" + "\n❤【今日份天气】\n❤城市： " + parent + city + \
                "\n❤日期： " + date + "\n❤星期: " + week + "\n❤天气: " + weather_type + "\n❤温度: " + wendu_high + " / " + wendu_low + "\n❤湿度: " + \
                shidu + "\n❤PM25: " + pm25 + "\n❤PM10: " + pm10 + "\n❤空气质量: " + quality + \
                "\n❤风力风向: " + fx + fl + "\n❤感冒指数: " + ganmao + "\n❤温馨提示： " + tips + "\n❤更新时间: " + update_time
         print (tdwt)
-        data = {
-            'msg': tdwt.encode ('utf-8')
-        }
-        requests.post (cpurl, data = data)  # 把天气数据转换成UTF-8格式，不然要报错。
+        #data = {
+        #   'msg': tdwt.encode ('utf-8')
+        #}
+        # requests.post (cpurl, data = data)  # 把天气数据转换成UTF-8格式，不然要报错。
     return parent, city, date, week, weather_type, weather_type, weather_type, shidu,pm25, pm10, quality, fx, fl, ganmao, tips, update_time
 
 
