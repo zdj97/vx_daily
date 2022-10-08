caeffeda1b8e01fac37da21102758adb07b08732
zdj97 <113007896+zdj97@users.noreply.github.com>
2022-10-08 14:55:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
 import os
 import random
+from bs4 import BeautifulSoup
+
 
 today = datetime.now()
 start_date = os.environ['START_DATE']
@@ -16,6 +18,58 @@ app_secret = os.environ["APP_SECRET"]
 
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
+city_code = '101230508'  # 进入https://where.heweather.com/index.html查询你的城市代码
+api = 'http://t.weather.itboy.net/api/weather/city/'  # API地址，必须配合城市代码使用
+
+
+
+def get_iciba_everyday ():
+    icbapi = 'http://open.iciba.com/dsapi/'
+    eed = requests.get (icbapi)
+    bee = eed.json ()  # 返回的数据
+    english = bee['content']
+    zh_CN = bee['note']
+    str = '【奇怪的知识】\n' + english + '\n' + zh_CN
+    return str
+
+def get_weather_new():
+    tqurl = api + city_code
+    response = requests.get (tqurl)
+    d = response.json ()  # 将数据以json形式返回，这个d就是返回的json数据
+    # print(d)
+    if (d['status'] == 200):  # 当返回状态码为200，输出天气状况
+        parent = d["cityInfo"]["parent"]  # 省
+        city = d["cityInfo"]["city"]  # 市
+        update_time = d["time"]  # 更新时间
+        date = d["data"]["forecast"][0]["ymd"]  # 日期
+        week = d["data"]["forecast"][0]["week"]  # 星期
+        weather_type = d["data"]["forecast"][0]["type"]  # 天气
+        wendu_high = d["data"]["forecast"][0]["high"]  # 最高温度
+        wendu_low = d["data"]["forecast"][0]["low"]  # 最低温度
+        shidu = d["data"]["shidu"]  # 湿度
+        pm25 = str (d["data"]["pm25"])  # PM2.5
+        pm10 = str (d["data"]["pm10"])  # PM10
+        quality = d["data"]["quality"]  # 天气质量
+        fx = d["data"]["forecast"][0]["fx"]  # 风向
+        fl = d["data"]["forecast"][0]["fl"]  # 风力
+        ganmao = d["data"]["ganmao"]  # 感冒指数
+        tips = d["data"]["forecast"][0]["notice"]  # 温馨提示
+        cpurl = 'https://qmsg.zendee.cn/send/' + spkey  # 自己改发送方式，我专门创建了个群来收消息，所以我用的group
+        # 天气提示内容
+        tdwt = get_iciba_everyday () + "\n-----------------------------------------" + "\n【今日份天气】\n城市： " + parent + city + \
+               "\n日期： " + date + "\n星期: " + week + "\n天气: " + weather_type + "\n温度: " + wendu_high + " / " + wendu_low + "\n湿度: " + \
+               shidu + "\nPM25: " + pm25 + "\nPM10: " + pm10 + "\n空气质量: " + quality + \
+               "\n风力风向: " + fx + fl + "\n感冒指数: " + ganmao + "\n温馨提示： " + tips + "\n更新时间: " + update_time
+        print (tdwt)
+        data = {
+            'msg': tdwt.encode ('utf-8')
+        }
+        requests.post (cpurl, data = data)  # 把天气数据转换成UTF-8格式，不然要报错。
+    return tdwt
+
+
+
+
 
 
 def get_weather():
@@ -39,6 +93,21 @@ def get_words():
   if words.status_code != 200:
     return get_words()
   return words.json()['data']['text']
+
+def getloverwords ():
+    texts = []
+    for i in range (1, int (random.randint (3, 83))):
+        url = 'https://www.duanwenxue.com/huayu/tianyanmiyu/list_{}.html'.format (i)
+        response = requests.get (url)
+        texts.append (response.text)
+    articles = []
+    for text in texts:
+        soup = BeautifulSoup (text, 'lxml')
+        arttis = soup.find ('div', class_ = 'list-short-article').find_all ('a', {'target': "_blank"})  # 寻找情话内容
+        #  通过列表推导式以及for循环获取到每个a标签里面的text内容并通过strip去除空格
+        articles.extend ([arttis[i].text.strip () for i in range (len (arttis))])
+    todaywords = articles[random.randint (0, len (articles) - 1)]  # 随机选取其中一条情话
+    return todaywords
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
